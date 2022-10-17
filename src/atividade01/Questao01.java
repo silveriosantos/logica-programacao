d973c3ce2107f0f7c77454d6676b9d509284a949
Silverio Dos Santos <silveriojannard@gmail.com>
2022-10-17 20:52:27 -0300

    Adicionei a recuperação

--- src/atividade01/Questao01.java
+++ src/atividade01/Questao01.java
@@ -17,18 +17,19 @@ public class Questao01 {
         double num2 = ler.nextDouble();
         double soma = num1 + num2;
         media = soma / 2;    
-        System.out.println("O resultado de nota 1 e nota 2 = "+media);
+        System.out.println("A media das duas notas e = "+media);
 
         if (media >= 7) {
-            System.out.println("Aluno Aprovado");
-            
-        } else  {
+            System.out.println("Aluno Aprovado");            
+        } else if (media >=5) {
+            System.out.println("Aluno em Recuperação");
+        }else {
             System.out.println("Aluno Reprovado");
-            
-            ler.close();
-            
+        }       
+
+            ler.close();            
         }
         
                      
     }
-}
+
