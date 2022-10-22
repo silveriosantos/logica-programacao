4114ac42a0477fe200819a1a9cab4adefafc1a39
Silverio Dos Santos <silveriojannard@gmail.com>
2022-10-21 23:44:59 -0300

    Atividades03

---
+++ src/atividade03/Questao05.java
@@ -0,0 +1,12 @@
+package atividade03;
+
+import java.util.Scanner;
+
+public class Questao05 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+    }
+
+    
+}
