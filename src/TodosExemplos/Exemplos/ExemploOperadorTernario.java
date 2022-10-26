78d977d8980000306301b6f31570df42a0cb7cb5
Silverio Dos Santos <silveriojannard@gmail.com>
2022-10-25 22:53:49 -0300

    Organização de Pastas

---
+++ src/TodosExemplos/Exemplos/ExemploOperadorTernario.java
@@ -0,0 +1,8 @@
+package TodosExemplos.Exemplos;
+public class ExemploOperadorTernario {
+    public static void main(String[] args) {
+        int hora = 12;
+        String turno = (hora >= 18) ? "Noite" : "Dia";
+        System.out.println(turno);
+    }
+}
