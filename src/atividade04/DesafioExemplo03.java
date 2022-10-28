bc8aab7037dee3995e94dfb3338afc03d6343ef6
Silverio Dos Santos <silveriojannard@gmail.com>
2022-10-27 23:11:06 -0300

    ResolvendoDesafio

--- src/atividade04/DesafioExemplo03.java
+++ src/atividade04/DesafioExemplo03.java
@@ -9,71 +9,91 @@ public class DesafioExemplo03 {
         // arraylista é uma classe java mais flexivel para manipular uma lista
         ArrayList<String> lista = new ArrayList<String>();
         int selecionado = 0;
-        Object[] opcoes = { "Incluir", "Listar", "Apagar","alterar", "Finalizar" };
+        Object[] opcoes = { "Incluir", "Listar", "Apagar", "Alterar" };
         do {
-            selecionado = JOptionPane.showOptionDialog(
-                    null,
-                    "O que você deseja?",
-                    "Exemplo",
-                    JOptionPane.DEFAULT_OPTION,
-                    JOptionPane.WARNING_MESSAGE,
-                    null, opcoes, opcoes[0]);
-            if (selecionado == 0) { // opcoes[selecionado].equals("Incluir")
-                String item = JOptionPane.showInputDialog("Digite");
-                lista.add(item); // guarda na lista o item digitado
-            }
-            if (selecionado == 1) {
-                String msg = "";
-                for (String item : lista) {
-                    msg += item + "\n";
-                }
-                JOptionPane.showMessageDialog(null, msg);
-            }
-            if (selecionado == 2) {
-                Object[] opcaoApagar = new Object[lista.size()];
-                for (int i = 0; i < opcaoApagar.length; i++) {
-                    opcaoApagar[i] = lista.get(i);
-                }
-                Object itemSelecionado = JOptionPane.showInputDialog(
+            try {
+                selecionado = JOptionPane.showOptionDialog(
                         null,
-                        "Selecione o item para apagar",
-                        "Apagar",
-                        JOptionPane.INFORMATION_MESSAGE,
-                        null,
-                        opcaoApagar,
-                        opcaoApagar[0]);
-                // System.out.println(itemSelecionado.toString());
-                for (int i = 0; i < lista.size(); i++) {
-                    if (itemSelecionado.toString().equals(lista.get(i))) {
-                        lista.remove(i);
+                        "O que você deseja?",
+                        "Exemplo",
+                        JOptionPane.DEFAULT_OPTION,
+                        JOptionPane.WARNING_MESSAGE,
+                        null, opcoes, opcoes[0]);
+                // bloco incluir
+                if (selecionado == 0) { // opcoes[selecionado].equals("Incluir")
+                    String item = JOptionPane.showInputDialog("Digite");
+                    if (item != null) {
+                        lista.add(item); // guarda na lista o item digitado
                     }
                 }
+                // bloco listar
+                if (selecionado == 1) {
+                    String msg = "";
+                    for (String item : lista) {
+                        msg += item + "\n";
+                    }
+                    if (msg.equals("")) {
+                        msg = "\"Lista sem registro\"";
+                    }
+                    JOptionPane.showMessageDialog(null, msg);
+                }
+                // bloco apagar
+                if (selecionado == 2 && lista.size() > 0) {
+                    Object[] opcaoApagar = new Object[lista.size()];
+                    for (int i = 0; i < opcaoApagar.length; i++) {
+                        opcaoApagar[i] = lista.get(i);
+                    }
+                    Object itemSelecionado = JOptionPane.showInputDialog(
+                            null,
+                            "Selecione o item para apagar",
+                            "Apagar",
+                            JOptionPane.INFORMATION_MESSAGE,
+                            null,
+                            opcaoApagar,
+                            opcaoApagar[0]);
+                    // System.out.println(itemSelecionado.toString());
+                    for (int i = 0; i < lista.size(); i++) {
+                        if (itemSelecionado != null && itemSelecionado.equals(lista.get(i))) {
+                            int confirmacao = JOptionPane.showConfirmDialog(null,
+                                    String.format("Tem certeza que quer apagar o item \"%s\"?", lista.get(i)));
+                            if (confirmacao == 0) {
+                                lista.remove(i);
+                            }
+                        }
+                    }
+                }
+                // bloco alterar
+                if (selecionado == 3) {
+                    if (lista.size() > 0) {
+                        Object[] opcoesAlterar = new Object[lista.size()];
+                        for (int i = 0; i < opcoesAlterar.length; i++) {
+                            opcoesAlterar[i] = lista.get(i);
+                        }
+                        Object itemSelecionado = JOptionPane.showInputDialog(
+                                null,
+                                "Selecione um item para alterar",
+                                "Alterar",
+                                JOptionPane.INFORMATION_MESSAGE,
+                                null,
+                                opcoesAlterar,
+                                opcoesAlterar[0]);
+                        for (int i = 0; i < lista.size(); i++) {
+                            if (itemSelecionado != null && itemSelecionado.equals(lista.get(i))) {
+                                String item = JOptionPane.showInputDialog("Digite o novo item", lista.get(i));
+                                lista.set(i, item);
+                            }
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Não possivel fazer alteração nenhum item na lista.");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace(); // plota o erro
+                // o try catch serve para evitar que o programa seja interrompido
+                // inesperadamente.
+                JOptionPane.showMessageDialog(null, "Erro inesperado identificado com try-catch");
             }
-            if (selecionado == 3) {
-                Object[] opcaoApagar = new Object[lista.size()];
-                for (int i = 0; i < opcaoApagar.length; i++) {
-                    opcaoApagar[i] = lista.get(i);
-                
-            }
-            Object itemSelecionado = JOptionPane.showInputDialog(
-                null,
-                "Selecione o item para alterar",
-                "alterar",
-                JOptionPane.INFORMATION_MESSAGE,
-                null,
-                opcaoApagar,
-                opcaoApagar[0]);
-                System.out.println(itemSelecionado.toString());
-            for (int i = 0; i < lista.size(); i++) {
-                if (itemSelecionado.toString().equals(lista.get(i))) {
-                    lista.remove(i);
+        } while (selecionado != -1);
 
-                    String item = JOptionPane.showInputDialog("Digite");
-                    lista.add(item);
-        } 
-
-    }
-}
-        }while (selecionado != 4);
     }
 }
