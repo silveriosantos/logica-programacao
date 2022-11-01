897b15e673dbf80110fba5b6257075ec82a918d6
Silverio Dos Santos <silveriojannard@gmail.com>
2022-10-31 21:36:40 -0300

    Exemplo POO Finalizado

---
+++ src/TodosExemplos/ExemplosOOP/Exemplo01.java
@@ -0,0 +1,19 @@
+package TodosExemplos.ExemplosOOP;
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        Caneta caneta1 = new Caneta("BIC","Azul");
+        caneta1.printStatus();
+        caneta1.rabiscar();
+        caneta1.printStatus();
+        caneta1.destampar();
+        caneta1.rabiscar();
+        caneta1.printStatus();
+        caneta1.escrever("Luiz Felipe Freire Maia");
+        caneta1.printStatus();
+        caneta1.escrever("Luiz");
+        caneta1.printStatus();
+        Caneta caneta2 = new Caneta("BIC", "Preta");
+        caneta2.printStatus();
+    }
+}
